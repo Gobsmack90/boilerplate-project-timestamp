349d0ab8314b213ec7ebfe43b1bfd2eb0b37a530
Harrison King <authorharrisonking@gmail.com>
2021-02-11 15:54:36 -0700

    final

--- server.js
+++ server.js
@@ -18,23 +18,60 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+//this is the work i had started with. I didn't finish it because I didn't fully understand how the Date() object and it's methods worked, and after hours of researching i still wasn't getting any closer, so i finally looked at the hints and provided solution, and at that point understood what needed to be done and how to do it. and from there there's no reason not to just use the provided solution. 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// const dateMiddleware = (req, res, next) => {
+//   //store recieved data
+//   const { reqData } = req.params;
+
+//   let isValidDate = 
+
+//   if () {
+//     req.unix = Date.toUTCString(reqData);
+//     req.utc = Date.valueOf(reqData);
+//     next();
+//   } else {
+//     req.error = "Invalid Date";
+//     next();
+//   }
+//   date = new Date().toString();
+
+// };
+
+// app.get('/api/timestamp/:date', dateMiddleware, (req, res) => {
+//   if (req.error) {
+//     res.send({error: req.error})
+//   } else {
+//     res.send({
+//       unix: req.unix,
+//       utc: req.utc
+//     });
+//   }
+// })
+
+app.get("/api/timestamp/", (req, res) => {
+  res.json({ unix: Date.now(), utc: Date() });
 });
 
-const dateMiddleware = (req, res, next) => {
-  const { date } = req.params;
-  date = new Date().toString();
-  next();
-};
+app.get("/api/timestamp/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
 
-app.get('/api/timestamp/:date', dateMiddleware, (req, res) => {
-  res.send({
-    date: req.date
-  });
-})
+  //A 4 digit number is a valid ISO-8601 for the beginning of that year
+  //5 digits or more must be a unix time, until we reach a year 10,000 problem
+  if (/\d{5,}/.test(dateString)) {
+    const dateInt = parseInt(dateString);
+    //Date regards numbers as unix timestamps, strings are processed differently
+    res.json({ unix: dateInt, utc: new Date(dateInt).toUTCString() });
+  } else {
+    let dateObject = new Date(dateString);
+
+    if (dateObject.toString() === "Invalid Date") {
+      res.json({ error: "Invalid Date" });
+    } else {
+      res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+    }
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
